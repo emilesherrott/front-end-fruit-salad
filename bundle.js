6dee610f6e6a40568aafe99c0ad01bbeda5acb1b
“emilesherrott” <emilesherrott@gmail.com>
2023-04-12 17:08:52 +0100

    async/await

--- bundle.js
+++ bundle.js
@@ -13,20 +13,21 @@ function extractFruit(e) {
     e.target.fruitInput.value = ""
 }
 
-function fetchFruitData(fruit) {
-    fetch(`https://fruity-api.onrender.com/fruits/${fruit}`)
-        .then(processResponse)
-        .then(data => addFruit(data))
-        .catch(e => console.log(e))
-}
-
-function processResponse(resp){
-    if(resp.ok){
-        return resp.json()
-    } else {
-        throw "Item not found in the fruit API"
+async function fetchFruitData(fruit) {
+    try {
+        const response = await fetch(`https://fruity-api.onrender.com/fruits/${fruit}`)
+        if(response.ok){
+            const data = await response.json()
+            addFruit(data)
+        } else {
+            throw "Error not a  fruit! Status code = " + response.status
+        }
+    } catch (e) {
+        console.log(e)
     }
 }
+
+
 
 function addFruit(fruit) {
     console.log(fruit)
@@ -36,8 +37,7 @@ function addFruit(fruit) {
     fruitList.appendChild(li)
 
     protein += Math.round(fruit['nutritions']['protein'] * 10) / 10
-    // rounded = Math.round(number * 10) / 10
-    fruitNutrition.textContent = "The total amount of protien in your fruit salad is: " + protein
+    fruitNutrition.textContent = "The total amount of protien in your fruit salad is: " + protein + "g"
 }
 
 function removeFruit(e) {
